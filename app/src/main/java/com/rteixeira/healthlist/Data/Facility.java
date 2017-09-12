1cb5808d28699948d1a780eebf08fd3f862899f4
Raul Teixeira <rjt@noniussoftware.com>
2017-09-12 17:40:31 +0100

    inital commit; probable file structure

---
+++ app/src/main/java/com/rteixeira/healthlist/Data/Facility.java
@@ -0,0 +1,8 @@
+package com.rteixeira.healthlist.Data;
+
+/**
+ * Created by Raul Teixeira on 12/09/2017.
+ */
+
+public class Facility {
+}
---
+++ app/src/main/java/com/rteixeira/healthlist/Data/source/ListDataSource.java
@@ -0,0 +1,8 @@
+package com.rteixeira.healthlist.Data.source;
+
+/**
+ * Created by Raul Teixeira on 12/09/2017.
+ */
+
+public class ListDataSource {
+}
